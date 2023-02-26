9d0f2887649425c1e23f40a5a0977351c08e21ff
Mikhail <119260605+milaxcoo@users.noreply.github.com>
2023-02-26 08:02:03 +0000

    commit

--- bot.py
+++ bot.py
@@ -11,17 +11,10 @@ def start(message):
 
 
 
-#find random image for each request
+#find random image by keyword from google for each request
 @bot.message_handler(content_types=['text'])
 def send_image(message):
-    if message.chat.type == 'private':
-        if message.text == 'cat':
-            bot.send_photo(message.chat.id, open('cat.jpg', 'rb'))
-        elif message.text == 'dog':
-            bot.send_photo(message.chat.id, open('dog.jpg', 'rb'))
-        else:
-            bot.send_message(message.chat.id, 'I don\'t understand you.')
-
-
+    bot.send_message(message.chat.id, 'I am searching for image...')
+    bot.send_photo(message.chat.id, 'https://source.unsplash.com/random/800x600')
 
 bot.infinity_polling()
