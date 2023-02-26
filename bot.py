8fde99e8382abc58ad91624daa81404a7972e250
Mikhail <119260605+milaxcoo@users.noreply.github.com>
2023-02-26 05:52:28 +0000

    4

--- bot.py
+++ bot.py
@@ -5,7 +5,6 @@ bot = telebot.TeleBot(config.TOKEN)
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, 'Hello')
+    bot.send_message(message.chat.id, 'Привет, я бот, который умеет считать слова в тексте. Отправь мне текст, а я посчитаю слова в нем.')
 
-if __name__ == '__main__':
-    bot.polling(none_stop=True)
+bot.polling(none_stop=True)
