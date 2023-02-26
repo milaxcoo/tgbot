7b169ce565a5614065533c27ce4e4f90030f2d8e
Mikhail <119260605+milaxcoo@users.noreply.github.com>
2023-02-26 09:05:47 +0000

    comiit

--- bot.py
+++ bot.py
@@ -1,8 +1,6 @@
 import telebot
 import config
-#import flask
 import requests
-
 
 
 bot = telebot.TeleBot(config.TOKEN)
@@ -12,17 +10,13 @@ def start(message):
     #welcome
     bot.send_message(message.chat.id, 'Hello, {0.first_name}!\nI am - <b>{1.first_name}</b>, the bot created to find pics fo u.'.format(message.from_user, bot.get_me()), parse_mode='html')
 
+#get random image
+@bot.message_handler(commands=['get'])
+def get(message):
+    r = requests.get('https://aws.random.cat/meow')
+    url = r.json()['file']
+    bot.send_photo(message.chat.id, url)
 
-
-#find random image for every message
-@bot.message_handler(content_types=['text'])
-def send_image(message):
-    bot.send_photo(message.chat.id, 'https://picsum.photos/200/300/?random')
-
-@bot.message_handler(commands=['random'])
-def send_random_pic(message):
-    response = requests.get('https://source.unsplash.com/random')
-    bot.send_photo(message.chat.id, response.content)
 
 
 bot.infinity_polling()
