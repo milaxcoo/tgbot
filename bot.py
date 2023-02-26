0fed8e3be6a642532dc40595d0a4aecc424950b0
Mikhail <119260605+milaxcoo@users.noreply.github.com>
2023-02-26 09:24:16 +0000

    lol

--- bot.py
+++ bot.py
@@ -5,10 +5,10 @@ import requests
 
 bot = telebot.TeleBot(config.TOKEN)
 
-@bot.message_handler(commands=['start'])
+@bot.message_handler(commands=['start', 'help'])
 def start(message):
     #welcome
-    bot.send_message(message.chat.id, 'Hello, {0.first_name}!\nI am - <b>{1.first_name}</b>, the bot created to find pics fo u.'.format(message.from_user, bot.get_me()), parse_mode='html')
+    bot.send_message(message.chat.id, 'Hello, {0.first_name}!\nI am - <b>{1.first_name}</b>, the bot created to find pics fo u!\n Just type only one keyword!'.format(message.from_user, bot.get_me()), parse_mode='html')
 
 #get random image
 @bot.message_handler(commands=['randomcat'])
@@ -20,11 +20,10 @@ def get(message):
 #get image by keyword
 @bot.message_handler(content_types=['text'])
 def gett(message):
-    r = requests.get('https://source.unsplash.com/random/?{0}' + message.text)
+    r 
+    r = requests.get('https://source.unsplash.com/random/?{0}', message.text)
     url = r.url
     bot.send_photo(message.chat.id, url)
-
-
 
 
 bot.infinity_polling()
