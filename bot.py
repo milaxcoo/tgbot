4bd2b09497b304cce0d8552eb2da0a145c538958
Mikhail <119260605+milaxcoo@users.noreply.github.com>
2023-02-26 07:02:33 +0000

    commit

--- bot.py
+++ bot.py
@@ -7,8 +7,13 @@ bot = telebot.TeleBot(config.TOKEN)
 def start(message):
     bot.reply_to(message, "Sup, bro?")
 
-@bot.message_handler(func=lambda message: True)
-def echo(message):
-    bot.send_message(message.chat.id, message.text)
+@bot.inline_handler(lambda query: query.query == 'text')
+def query_text(inline_query):
+    try:
+        r = types.InlineQueryResultArticle('1', 'Result', types.InputTextMessageContent('Result message.'))
+        r2 = types.InlineQueryResultArticle('2', 'Result2', types.InputTextMessageContent('Result message2.'))
+        bot.answer_inline_query(inline_query.id, [r, r2])
+    except Exception as e:
+        print(e)
 
 bot.infinity_polling()
