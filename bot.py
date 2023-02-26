5287f510b58b9bc4e2981638a53dc8c04f68a63e
Mikhail <119260605+milaxcoo@users.noreply.github.com>
2023-02-26 06:30:15 +0000

    la

--- bot.py
+++ bot.py
@@ -7,11 +7,5 @@ bot = telebot.TeleBot(config.TOKEN)
 def start(message):
     bot.reply_to(message, "Sup, bro?")
 
-
-@bot.message_handler(func=lambda m: True)
-def echo(message):
-    bot.reply_to(message, message.text)
-
-
 bot.polling()
 #bot.infinity_polling()
