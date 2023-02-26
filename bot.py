d18aa91e69996894e20c30912a408707f2a663b7
Mikhail <119260605+milaxcoo@users.noreply.github.com>
2023-02-26 07:56:36 +0000

    vommit

--- bot.py
+++ bot.py
@@ -1,21 +1,22 @@
 import telebot
 import config
 
-from telebot import types
 
 bot = telebot.TeleBot(config.TOKEN)
 
 @bot.message_handler(commands=['start'])
 def start(message):
     #welcome
-    bot.send_message(message.chat.id, 'Привет, {0.first_name}!\nЯ - <b>{1.first_name}</b>, помогу тебе с поиском картинки.'.format(message.from_user, bot.get_me()), parse_mode='html')
+    bot.send_message(message.chat.id, 'Hello, {0.first_name}!\nI am - <b>{1.first_name}</b>, the bot created to find pics fo u.'.format(message.from_user, bot.get_me()), parse_mode='html')
+
 
 
 #find image from google by keyword
 @bot.message_handler(content_types=['text'])
-def lalala(message):
-    bot.send_photo(message.chat.id, 'https://source.unsplash.com/random/?{0}'.format(message.text))
+def find_image(message):
+    bot.send_message(message.chat.id, 'Searching for image...')
+    bot.send_photo(message.chat.id, 'https://www.google.com/search?q={0}&tbm=isch'.format(message.text))
 
-
+    
 
 bot.infinity_polling()
