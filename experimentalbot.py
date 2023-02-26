9a3b95b1c5ef3f568f617fa0187d83f14de720d3
Mikhail <119260605+milaxcoo@users.noreply.github.com>
2023-02-26 09:37:21 +0000

    version 1.0.1

--- experimentalbot.py
+++ experimentalbot.py
@@ -10,12 +10,18 @@ def start(message):
     #welcome
     bot.send_message(message.chat.id, 'Hello, {0.first_name}!\nI am - <b>{1.first_name}</b>, the bot created to find pics fo u!\nJust type only one keyword!'.format(message.from_user, bot.get_me()), parse_mode='html')
 
+#get random image
+@bot.message_handler(commands=['randomcat'])
+def get(message):
+    r = requests.get('https://aws.random.cat/meow', timeout=5)
+    url = r.json()['file']
+    bot.send_photo(message.chat.id, url)
 
+#get image by keyword
 @bot.message_handler(content_types=['text'])
 def image(message):
     r = requests.get('https://source.unsplash.com/random/?{0}'.format(message.text), timeout=5)
     url = r.url
     bot.send_photo(message.chat.id, url)
-
 
 bot.infinity_polling()
